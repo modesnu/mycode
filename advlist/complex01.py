48a8a3888653913774d6843b60c70d5e34eac69e
Picasso <picasso@users.noreply.github.com>
2022-08-23 15:24:26 +0000

    join and split

---
+++ advlist/complex01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main():
+    # create a list called list1
+    list1 = ["cisco_nxos", "arista_eos", "cisco_ios"]
+
+    # display list1
+    print(list1)
+
+main()
+
